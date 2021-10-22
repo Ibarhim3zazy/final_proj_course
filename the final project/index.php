9bd379ac7b9b7b9f116544dc3da04f2f31248bb1
Ibrahim 3zazy <83598736+Ibarhim3zazy@users.noreply.github.com>
2021-10-22 23:41:10 +0200

    Add files via upload

---
+++ the final project/index.php
@@ -0,0 +1,160 @@
+<html>
+<head>
+ <meta charset = "utf-8">
+    <meta name = "description" content = "welcome to my website">
+    <meta name = "viewport" content = "width=device-width,intial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+   </head>
+  <body>
+  <?php
+    include('slideshow.php');
+    include('header.php');	?>
+    <div class="container">
+     <h2>Top selling items</h2>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <br style="clear: both;">
+    </div>
+    <div class="ads">
+     <input type="button" value="x">
+     ADS
+    </div>
+    <div class="container">
+      <div class="container_header red">
+        <h2>Flash Sales Every day</h2><a href="#">See All &#10095;</a>
+      </div>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <br style="clear: both;">
+    </div>
+    <div class="container">
+      <div class="container_header">
+        <h2>New On <b style="font-size: 25px;">e</b> Store</h2><a href="#">See All &#10095;</a>
+      </div>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <br style="clear: both;">
+    </div>
+    <div class="container">
+      <div class="container_header">
+        <h2>Recently viewed items</h2><a href="#">See All &#10095;</a>
+      </div>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <div class="products">
+       <img src="img/prod4.jpg" alt="prod">
+       <span>item1</span>
+       <h3>EGP 600</h3>
+       <h4>EGP 400</h4>
+     </div>
+     <br style="clear: both;">
+    </div>
+    <?php require 'footer.php'; ?>
+  </body>
+  </html>
