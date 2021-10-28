ba8e9d9ff6c2a7f64c66d4c21578afb916be30dc
Ibrahim 3zazy <83598736+Ibarhim3zazy@users.noreply.github.com>
2021-10-28 12:43:14 +0200

    Delete the final project directory

--- the final project/connection.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-  $con= new mysqli("localhost", "root", "", "e_store_clothes");
-  if ($con ->connect_errno) {
-    die("connection error");
-    // echo "connection error";
-    // exit();
-  };
- ?>
